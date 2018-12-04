35c271457c77f3a22c1c26bd0ebeb3a01828796c
Lee Behan <rc0032865@student.riversidecollege.ac.uk>
2018-12-04 10:52:50 +0000

    Double sin / double inverse sin

--- Forces/Form1.cs
+++ Forces/Form1.cs
@@ -16,5 +16,60 @@ namespace Forces
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double A = double.Parse(textBox2.Text);
+                double ans = sin(A);
+                label1.Text = ans.ToString();
+
+            }
+            catch
+            {
+                MessageBox.Show("Enter a NUMBER");
+            }
+        }
+
+        double sin(double A)
+        {
+            double rad = A / 180.0 * Math.PI;
+            return Math.Sin(rad);
+        }
+
+        double inversesin(double A)
+        {
+            double rad = A / 180.0 * Math.PI;
+            return Math.Asin(rad);
+        }
+
+        double cos(double A)
+        {
+            double rad = A / 180.0 * Math.PI;
+            return Math.Cos(rad);
+        }
+
+
+        double inversecos(double A);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+ 
